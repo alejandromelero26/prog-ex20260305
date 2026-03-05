8708043cd99e0bdf7c61b5c80bd489ac255abe86
Alejandro <amelevel@myuax.com>
2026-03-05 09:09:12 +0100

    Creada la clase CentroFP y los metodos registrarAlumno y buscarPrimerHuecoLibre

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -3,7 +3,6 @@ public class CentroFP {
     private final int MAX_ALUMNOS;
 
     public CentroFP(int cantidad_maxima_alumnos) {
-        // Validación de número positivo
         if (cantidad_maxima_alumnos <= 0) {
             this.MAX_ALUMNOS = 20;
         } else {
@@ -15,7 +14,6 @@ public class CentroFP {
 
     public Alumno buscarAlumno(int id) {
         for (int i = 0; i < MAX_ALUMNOS; i++) {
-            // CORRECCIÓN: Comprobar que la POSICIÓN no sea null
             if (alumnos[i] != null && alumnos[i].getId() == id) {
                 return alumnos[i];
             }
@@ -23,6 +21,54 @@ public class CentroFP {
         return null;
     }
 
+    private int buscarPrimerHuecoLibre(){
+        int posicionLibre = -1;
+        boolean seguirBuscando = true;
+
+        for (int i = 0; i < MAX_ALUMNOS && seguirBuscando; i++) {
+            if(alumnos[i] == null){
+                posicionLibre = i;
+                seguirBuscando = false;
+            }
+        }
+        return posicionLibre;
+    }
+
+    public boolean registrarAlumno(Alumno alumno){
+        boolean alumnoAnadido = false;
+        int posicion;
+
+        if (buscarAlumno(alumno.getId()) == null) {
+            posicion = buscarPrimerHuecoLibre();
+            if (posicion >= 0) {
+                alumnos[posicion] = alumno;
+                alumnoAnadido = true;
+            }
+        }
+        return alumnoAnadido;
+    }
+
+    public String mostrarAlumno(){
+        StringBuilder sb = new StringBuilder("Alumnos :");
+        for (int i = 0; i < MAX_ALUMNOS; i++) {
+            if (alumnos[i] != null) {
+                sb.append(alumnos[i].toString());
+            }
+        }
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    public int contarAlumnos(){
+        int contador = 0;
+        for (int i = 0; i < MAX_ALUMNOS; i++) {
+            if (alumnos[i] != null) {
+                contador++;
+            }
+        }
+        return contador;
+    }
 }
 
 
