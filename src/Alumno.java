3024e3b9fa3d093c4b049fdf6e7dab365d540ec3
Alejandro <amelevel@myuax.com>
2026-03-05 08:45:42 +0100

    CREADA LA CLASE ALUMNO con el metodo alumno y los distintos getters

---
+++ src/Alumno.java
@@ -0,0 +1,25 @@
+public class Alumno {
+    int id;
+    String nombre;
+    String tituloFP;
+    int edad;
+
+    public Alumno(int id, String nombre, String tituloFP, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getTituloFP() {
+        return tituloFP;
+    }
+    public int getEdad() {
+        return edad;
+    }
+}
